a14ec579dc6ba2d22b25a6dfd6ce37b73daddfa8
dennislam4 <107871794+dennislam4@users.noreply.github.com>
2022-07-22 20:47:24 -0600

    Initial commit

---
+++ make_employee_dict.py
@@ -0,0 +1,47 @@
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 7/22/2022
+# Description: A class that contains employee's name, ID_number, salary, and email_address.
+# Also contains a funtion that take parameters a list of names, list of ID numbers, list of
+# salaries and a list of emails. The function creates a employee objects and  returns a dictionary
+# where the key is the ID number and the value is the employee objects created.
+
+class Employee:
+    """
+    Represents an employee's personal information with their name, ID number, salary and email
+    """
+    def __init__(self, name, id_number, salary, email_address):
+        """Initializes employee's personal information. """
+        self.__name = name
+        self.__id_number = id_number
+        self.__salary = salary
+        self.__email_address = email_address
+
+    def get_name(self):
+        """Returns the name of employee."""
+        return self.__name()
+
+    def get_id_number(self):
+        """Returns ID number of employee."""
+        return self.__id_number()
+
+    def get_salary(self):
+        """Returns salary of employee."""
+        return self.__salary()
+
+    def get_email_address(self):
+        """Returns the e-mail address of employee."""
+        return self.__email_address()
+
+
+def make_employee_dict(names, ids, sals, emails):
+    """
+    Function that creates employee objects and adds it to a dictionary where employee ID
+    number is the key and value is the created employee object.
+    """
+    emp_dic = {}
+    for data in range(len(names)):
+        employee = Employee(names[data], ids[data], sals[data], emails[data])
+        emp_dic[ids[data]] = employee
+    return emp_dic
+
